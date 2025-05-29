6a93bb913a9dc8d89b9432e8307b746d2ee22603
Matías Recabarren <mat.recabarren@duocuc.cl>
2025-05-29 05:03:52 -0400

    Base de Datos sirve

--- test_oracle.py
+++
@@ -1,8 +0,0 @@
-import oracledb
-
-connection = oracledb.connect(
-    user='tu_usuario',
-    password='tu_password',
-    dsn='localhost/ORCL'
-)
-print("Conexión exitosa:", connection.oracle_version)
