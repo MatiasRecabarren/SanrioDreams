ca296c1eeb25f44f12c53ae42ea380f6f2305e8e
MatiasRecabarren <mat.recabarren@duocuc.cl>
2026-05-13 12:29:09 -0400

    Agregando STATICFILES_DIRS y configuracion de media

--- web/settings.py
+++ web/settings.py
@@ -88,11 +88,18 @@ TIME_ZONE = 'America/Santiago'
 USE_I18N = True
 USE_TZ = True
 
+
 # Archivos estáticos y media
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
